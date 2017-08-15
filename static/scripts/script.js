3ba03510505ab34395798106369169ad78e881cc
Flawid DSouza <flawid@live.in>
2017-08-15 15:22:50 +0530

    Changed code order
    Now playlist-item & playlists template come before video-item & videos template

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -74,6 +74,174 @@ function dynamicSort(property) { // giving this a -property will sort in descend
     }
 }
 
+Vue.component('playlist-item', {
+    props: ['playlist'],
+    template: `
+        <router-link :to="'/videos/' + playlist.id + '/' + playlist.name.replace('?','')" :title="playlist.description" class="playlist" v-if="playlist.id">
+            {{ playlist.name }}
+        </router-link>
+        <div class="playlist" v-else>
+            {{ playlist.name }}
+        </div>
+    `
+})
+
+Vue.component('playlists', {
+    template: `
+        <div id="playlists" v-if="loading">Loading playlists...</div>
+        <div id="playlists" v-else-if="playlists.length > 0">
+            <div class="context-menu" ref="ctx">
+                <ul>
+                    <li @click="renamePlaylist(activePlaylist)">Rename</li>
+                    <li @click="deletePlaylist(activePlaylist)">Delete</li>
+                </ul>
+            </div>
+            <div class="fixed">
+                <div class="block-align-right">
+                    <button @click="createPlaylist()" class="playlist-add">Add Playlist</button>
+                </div>
+                <input type="text" placeholder="Type to filter playlists..." class="playlists-filter" v-on:input="filterPlaylists" v-model="playlistsFilter">
+                <div class="sorter">
+                    <select v-model="sorterValue" @change="sorterValueChange">
+                        <option disabled>Order by:</option>
+                        <option>Add Date</option>
+                        <option>Last Updated Date</option>
+                        <option>Name</option>
+                        <option>Last Added To</option>
+                    </select>
+                    <select v-model="sorterOrder" @change="sorterOrderChange">
+                        <option>Ascending</option>
+                        <option>Descending</option>
+                    </select>
+                </div>
+                <router-link to="/videos" class="playlist">All Videos</router-link>
+                <div class="separator"></div>
+                <playlist-item v-for="playlist in playlists" :key="playlist.id" :playlist="playlist" @contextmenu.prevent.native="showContextMenu($event, playlist)" v-click-outside="hideContextMenu" v-if="!playlist.noContextMenu"></playlist-item>
+                <playlist-item :playlist="playlist" class="cursor-default" v-else></playlist-item>
+            </div>
+        </div>
+        <div id="playlists" v-else>No Playlists Found</div>
+    `,
+    data() {
+        return {
+            sharedState: store.state,
+            loading: true,
+            activePlaylist: {},
+            playlistsFilter: '',
+            sorterValue: 'Last Added To',
+            sorterOrder: 'Descending'
+        }
+    },
+    computed: {
+        playlists() {
+            return this.sharedState.playlists
+        }
+    },
+    created() {
+        if(localStorage.getItem('playlistsSorterValue')) {
+            this.sorterValue = localStorage.getItem('playlistsSorterValue')
+        }
+        if(localStorage.getItem('playlistsSorterOrder')) {
+            this.sorterOrder = localStorage.getItem('playlistsSorterOrder')
+        }
+        this.fetchPlaylists()
+    },
+    methods: {
+        fetchPlaylists() {
+            axios.get('/playlists').then(response => {
+                store.setPlaylistsAction(response.data)
+                this.sortPlaylists()
+                this.loading = false
+            })
+        },
+        createPlaylist() {
+            let playlistName = prompt("Enter new playlist name")
+            if(playlistName) {
+                axios.post('/add-playlist', { name: playlistName }).then(response => {
+                    let result = response.data
+                    if(result.status == 'success') {
+                        this.fetchPlaylists()
+                    } else {
+                        alert(result.message)
+                    }
+                })
+            }
+        },
+        renamePlaylist(playlist) {
+            let playlistName = prompt("Enter new playlist name", playlist.name)
+            if(playlistName) {
+                axios.post('/edit-playlist', { name: playlistName, id: playlist.id }).then(response => {
+                    let result = response.data
+                    if(result.status == 'success') {
+                        this.fetchPlaylists()
+                        if(document.location.hash == `#/videos/${playlist.id}/${playlist.name}`) {
+                            router.push(`/videos/${playlist.id}/${playlistName}`)
+                        }
+                    } else {
+                        alert(result.message)
+                    }
+                })
+            }
+        },
+        deletePlaylist(playlist) {
+            if(confirm("Are you sure you want to delete this playlist? Deleting a playlist will also delete all the videos under it!")) {
+                axios.post('/delete-playlist', { id: playlist.id }).then(response => {
+                    let result = response.data
+                    if(result.status == 'success') {
+                        store.removePlaylistAction(playlist)
+                        if(document.location.hash == `#/videos/${playlist.id}/${playlist.name}`) {
+                            router.push('/videos')
+                        }
+                    } else {
+                        alert(result.message)
+                    }
+                })
+            }
+        },
+        showContextMenu(e, playlist) {
+            this.$refs.ctx.style.display = 'block'
+            this.$refs.ctx.style.left = e.pageX + "px"
+            this.$refs.ctx.style.top = e.pageY + "px"
+            this.activePlaylist = playlist
+        },
+        hideContextMenu() {
+            this.$refs.ctx.style.display = 'none'
+        },
+        filterPlaylists() {
+            store.filterPlaylistsAction(this.playlistsFilter)
+        },
+        sortPlaylists() {
+            if(this.sorterValue != 'Last Updated Date') {
+                this.sorterValueChange()
+            }
+        },
+        sorterValueChange() {
+            localStorage.setItem('playlistsSorterValue', this.sorterValue)
+            switch(this.sorterValue) {
+                case 'Add Date':
+                    store.sortPlaylistsAction(dynamicSort('-created_at'))
+                    break
+                case 'Last Updated Date':
+                    store.sortPlaylistsAction(dynamicSort('-updated_at'))
+                    break
+                case 'Name':
+                    store.sortPlaylistsAction(dynamicSort('-name'))
+                    break
+                case 'Last Added To':
+                    store.sortPlaylistsAction(dynamicSort('-updated_at:1'))
+                    break
+            }
+            if(this.sorterOrder == 'Ascending') {
+                store.reversePlaylistsAction()
+            }
+        },
+        sorterOrderChange() {
+            localStorage.setItem('playlistsSorterOrder', this.sorterOrder)
+            store.reversePlaylistsAction()
+        }
+    }
+})
+
 Vue.component('video-item', {
     props: ['video', 'index', 'playlists'],
     template: `
@@ -369,175 +537,6 @@ let videos = Vue.component('videos', {
         sorterOrderChange() {
             localStorage.setItem('videosSorterOrder', this.sorterOrder)
             this.videos.reverse()
-        }
-    }
-})
-
-Vue.component('playlist-item', {
-    props: ['playlist'],
-    template: `
-        <router-link :to="'/videos/' + playlist.id + '/' + playlist.name.replace('?','')" :title="playlist.description" class="playlist" v-if="playlist.id">
-            {{ playlist.name }}
-        </router-link>
-        <div class="playlist" v-else>
-            {{ playlist.name }}
-        </div>
-    `
-})
-
-Vue.component('playlists', {
-    template: `
-        <div id="playlists" v-if="loading">Loading playlists...</div>
-        <div id="playlists" v-else-if="playlists.length > 0">
-            <div class="context-menu" ref="ctx">
-                <ul>
-                    <li @click="renamePlaylist(activePlaylist)">Rename</li>
-                    <li @click="deletePlaylist(activePlaylist)">Delete</li>
-                </ul>
-            </div>
-            <div class="fixed">
-                <div class="block-align-right">
-                    <button @click="createPlaylist()" class="playlist-add">Add Playlist</button>
-                </div>
-                <input type="text" placeholder="Type to filter playlists..." class="playlists-filter" v-on:input="filterPlaylists" v-model="playlistsFilter">
-                <div class="sorter">
-                    <select v-model="sorterValue" @change="sorterValueChange">
-                        <option disabled>Order by:</option>
-                        <option>Add Date</option>
-                        <option>Last Updated Date</option>
-                        <option>Name</option>
-                        <option>Last Added To</option>
-                    </select>
-                    <select v-model="sorterOrder" @change="sorterOrderChange">
-                        <option>Ascending</option>
-                        <option>Descending</option>
-                    </select>
-                </div>
-                <router-link to="/videos" class="playlist">All Videos</router-link>
-                <div class="separator"></div>
-                <playlist-item v-for="playlist in playlists" :key="playlist.id" :playlist="playlist" @contextmenu.prevent.native="showContextMenu($event, playlist)" v-click-outside="hideContextMenu" v-if="!playlist.noContextMenu"></playlist-item>
-                <playlist-item :playlist="playlist" class="cursor-default" v-else></playlist-item>
-            </div>
-        </div>
-        <div id="playlists" v-else>No Playlists Found</div>
-    `,
-    data() {
-        return {
-            sharedState: store.state,
-            loading: true,
-            activePlaylist: {},
-            playlistsFilter: '',
-            sorterValue: 'Last Added To',
-            sorterOrder: 'Descending'
-        }
-    },
-    computed: {
-        playlists() {
-            return this.sharedState.playlists
-        }
-    },
-    created() {
-        if(localStorage.getItem('playlistsSorterValue')) {
-            this.sorterValue = localStorage.getItem('playlistsSorterValue')
-        }
-        if(localStorage.getItem('playlistsSorterOrder')) {
-            this.sorterOrder = localStorage.getItem('playlistsSorterOrder')
-        }
-        this.fetchPlaylists()
-    },
-    methods: {
-        fetchPlaylists() {
-            axios.get('/playlists').then(response => {
-                store.setPlaylistsAction(response.data)
-                this.sortPlaylists()
-                this.loading = false
-            })
-        },
-        createPlaylist() {
-            let playlistName = prompt("Enter new playlist name")
-            if(playlistName) {
-                axios.post('/add-playlist', { name: playlistName }).then(response => {
-                    let result = response.data
-                    if(result.status == 'success') {
-                        this.fetchPlaylists()
-                    } else {
-                        alert(result.message)
-                    }
-                })
-            }
-        },
-        renamePlaylist(playlist) {
-            let playlistName = prompt("Enter new playlist name", playlist.name)
-            if(playlistName) {
-                axios.post('/edit-playlist', { name: playlistName, id: playlist.id }).then(response => {
-                    let result = response.data
-                    if(result.status == 'success') {
-                        this.fetchPlaylists()
-                        if(document.location.hash == `#/videos/${playlist.id}/${playlist.name}`) {
-                            router.push(`/videos/${playlist.id}/${playlistName}`)
-                        }
-                    } else {
-                        alert(result.message)
-                    }
-                })
-            }
-        },
-        deletePlaylist(playlist) {
-            if(confirm("Are you sure you want to delete this playlist? Deleting a playlist will also delete all the videos under it!")) {
-                axios.post('/delete-playlist', { id: playlist.id }).then(response => {
-                    let result = response.data
-                    if(result.status == 'success') {
-                        store.removePlaylistAction(playlist)
-                        if(document.location.hash == `#/videos/${playlist.id}/${playlist.name}`) {
-                            router.push('/videos')
-                        }
-                    } else {
-                        alert(result.message)
-                    }
-                })
-                
-            }
-        },
-        showContextMenu(e, playlist) {
-            this.$refs.ctx.style.display = 'block'
-            this.$refs.ctx.style.left = e.pageX + "px"
-            this.$refs.ctx.style.top = e.pageY + "px"
-            this.activePlaylist = playlist
-        },
-        hideContextMenu() {
-            this.$refs.ctx.style.display = 'none'
-        },
-        filterPlaylists() {
-            store.filterPlaylistsAction(this.playlistsFilter)
-        },
-        sortPlaylists() {
-            if(this.sorterValue != 'Last Updated Date') {
-                this.sorterValueChange()
-            }
-        },
-        sorterValueChange() {
-            localStorage.setItem('playlistsSorterValue', this.sorterValue)
-            switch(this.sorterValue) {
-                case 'Add Date':
-                    store.sortPlaylistsAction(dynamicSort('-created_at'))
-                    break
-                case 'Last Updated Date':
-                    store.sortPlaylistsAction(dynamicSort('-updated_at'))
-                    break
-                case 'Name':
-                    store.sortPlaylistsAction(dynamicSort('-name'))
-                    break
-                case 'Last Added To':
-                    store.sortPlaylistsAction(dynamicSort('-updated_at:1'))
-                    break
-            }
-            if(this.sorterOrder == 'Ascending') {
-                store.reversePlaylistsAction()
-            }
-        },
-        sorterOrderChange() {
-            localStorage.setItem('playlistsSorterOrder', this.sorterOrder)
-            store.reversePlaylistsAction()
         }
     }
 })
